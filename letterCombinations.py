74db396bed99a72e3c66cf541187219b158ffa3d
YoussefEldoksh <youssefeldoksh788@gmail.com>
2026-06-18 04:30:22 +0300

    17

---
+++ letterCombinations.py
@@ -0,0 +1,34 @@
+
+def letterCombinations(digits: str):
+    letters = {
+        "2":"abc",
+        "3":"def",
+        "4":"ghi",
+        "5":"jkl",
+        "6":"mno",
+        "7":"pqrs",
+        "8":"tuv",
+        "9":"wxyz"
+        }
+    res = []
+
+    def backtrack(i, curStr):
+        if len(curStr) == len(digits):
+            res.append(curStr)
+            return
+        for c in letters[digits[i]]:
+            backtrack(i+1, curStr + c)
+        
+    backtrack(0,"")
+
+    return res
+        
+
+        
+        
+ 
+    
+
+    
+    
+print(letterCombinations("23"))
